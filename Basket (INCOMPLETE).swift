df0b823cf2331a9b80f88e1e1912fb5fafeb7256
WZYCX <chenalator2004@gmail.com>
2022-08-19 00:04:44 +0100

    Added total Cost price / order button
    -- Added 'Cost' variable that is used to display total cost of 'Current Basket'
    -- Added 'Order' button to show confirmation page before ordering basket
    -- Planned what the 'Current Basket' local array would look like
    -- Added Preview code for BasketPage()

--- Basket (INCOMPLETE).swift
+++ Basket (INCOMPLETE).swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct BasketPage: View{
     
     @EnvironmentObject var viewRouter: ViewRouter
+    @State var Cost = 0.00
     
     var body: some View{
             ZStack{
@@ -21,10 +22,41 @@ struct BasketPage: View{
                         }
                     }
                     Spacer()
+                    
+                    //Displays total cost of products
+                    //Cost = String(format: "%.2f", Cost) // round to two d.p. as a price
+                    Text("Total: £\(String(format: "%.2f", Cost))")
+                        .font(.system(size: 20, weight: .semibold))
+                        .multilineTextAlignment(.leading)
+                    
+                    //order button that leads to confirmation popup
+                    Button{
+                        print("Order")
+                    }label:{
+                        StdButton("Order")
+                    }
+                    
+                    Spacer()
+                        .frame(height:20)
+                    
                     Footer()
                 }
             
         }
+    }
+}
+
+//-- Local Store of Current Basket --
+//
+//[ Cost, [item1, quantity1] , [item2, quantity2] , [item3, quantity3] ]
+
+// if an item's quantity is zero, remove item
+
+
+struct BasketPreview: PreviewProvider {
+    
+    static var previews: some View {
+        BasketPage()
     }
 }
 
