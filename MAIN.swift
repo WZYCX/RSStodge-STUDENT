ab6d8ae73acf4d117275eda74a2b0ed37907a077
WZYCX <chenalator2004@gmail.com>
2022-10-13 20:13:12 +0100

    Removing 'FirestoreManager' since it is useless

--- MAIN.swift
+++ MAIN.swift
@@ -6,13 +6,13 @@ import Firebase
 struct Stodge: App {
     
     @StateObject var viewRouter = ViewRouter() //MotherView
-    @StateObject var firestoreManager = FirestoreManager() //Firebase --allows FirestoreManager to be accessed by app
     @StateObject var allitems = AllItems() // Menu+Basket
     @StateObject var category = Categories() // Landing+Menu
     @StateObject var showcategory = showCategories() // Landing+Menu
     @StateObject var basket = Basket() // Basket+Menu
+    @StateObject var orders = Orders() // Orders
     
-    //firebase stuff
+    //firebase initialisation
     init(){
         FirebaseApp.configure()
     }
@@ -21,12 +21,12 @@ struct Stodge: App {
         WindowGroup {
             MotherView()
                 .ignoresSafeArea() // the home bar is causing 'safe area' below footer bar
-                .environmentObject(firestoreManager) //sets it as an environment object to be accessed by app
                 .environmentObject(viewRouter)
                 .environmentObject(allitems)
                 .environmentObject(category)
                 .environmentObject(showcategory)
                 .environmentObject(basket)
+                .environmentObject(orders)
         }
     }
     
